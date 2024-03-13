8a12accfc249e6b7f9d201701e0d8220d7542ddc
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2024-03-13 13:12:55 -0400

    build: configure tooling
    * eslint
    * lint-staged
    * commitlint
    * prettier
    * husky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -8,5 +8,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
